5bb2d4513cb06c1e64c9ee81832f8033a4b2f7c5
Elias Leguizamon <elias.leguizamon1997@gmail.com>
2024-09-09 16:00:24 -0300

    enable cors fro all origins

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
 from fastapi import FastAPI, Response, status
+from fastapi.middleware.cors import CORSMiddleware
 from random import choice
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 damaged_systems = {
     "navigation": "NAV-01",
